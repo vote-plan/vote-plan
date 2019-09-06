b12f995d6fe2585961f9c7187467cd83e7f72be3
Mark Cottman-Fields <cofiem@gmail.com>
2019-09-06 20:45:46 +1000

    Update config for proper domain config.

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -21,7 +22,12 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'junit', // results will be saved as $outputDir/$browserName.xml
+      suite: '',
+      useBrowserName: true,
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
